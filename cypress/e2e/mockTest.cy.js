5b9fdd07cc342399312d7a1a2d7b090b6b6ac5f4
Aanchal <aanchalverma.av@outlook.com>
2025-02-21 14:15:42 +0530

    Added test for ui and API integration validation

--- cypress/e2e/mockTest.cy.js
+++ cypress/e2e/mockTest.cy.js
@@ -28,4 +28,48 @@ describe('Validating the APIs in the website App Demo', () => {
      cy.wait('@bookretrieval')
      cy.get('p').should('contain', 'Oops only 1 Book available')
   })
+
+  it('Integration test between UI and API', () => {
+      
+    cy.intercept({
+
+      method: 'GET',
+      url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+
+    },
+  {
+    body:[{
+      
+        "book_name": "RestAssured with Java",
+        "isbn": "LSA",
+        "aisle": "2303"
+    },
+    {
+        "book_name": "RestAssured with Java",
+        "isbn": "DSR",
+        "aisle": "2304"
+    },
+    {
+        "book_name": "RestAssured with Java",
+        "isbn": "RCD",
+        "aisle": "2305"
+    },
+    {
+        "book_name": "RestAssured with Java",
+        "isbn": "TSD",
+        "aisle": "2307"
+    
+
+    }]
+
+  }).as('bookretrievals')
+  
+  cy.contains('button','Virtual Library').click()
+  cy.wait('@bookretrievals').then((interception)=>{
+    cy.get('tr').should('have.length',interception.response.body.length+1)
+  })
+  cy.get('h2').should('contain', 'Books Availability in Rahul Shetty Academy Library')
+
+
+  })
 })
